7cf12eead7ef590ed85bd8da76b3a5a2748d473d
Kelsey <kelsey.mitchell@aggiemail.usu.edu>
2020-01-07 14:47:35 -0700

    separated functions of the model into modules for better readability and extention

--- model.py
+++ model.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from numpy import zeros
-from random import random
 
 from concentration import concentration
 from chemoattractant import chemoattractant
-
+from transition import transition
+from normal_transition import normal_transition
+from probability import probability
+from movement import movement
 
 size = 200
 workspace = zeros((size, size))
@@ -15,11 +17,12 @@ tip = 1
 source_m = 150
 source_n = 200
 k = .7
-p_mn = 0
 p_r = .25
 p_l = .25
 p_u = .25
 p_d = .25
+p_mn = 0
+probabilities = [p_r, p_l, p_u, p_d, p_mn]
 iterations = 0
 
 memory = set((m, n))
@@ -35,60 +38,12 @@ while iterations < 10000:
             conc = concentration(m+i[0], n+i[1], source_m, source_n)
         chemo.append(chemoattractant(conc, tip))
 
-    trans_Hplus = (1 / k) * (chemo[0] / (chemo[0] + chemo[4] + chemo[7] + chemo[11]))
-    trans_Hminus = (1 / k) * (chemo[0] / (chemo[0] + chemo[2] + chemo[6] + chemo[10]))
-    trans_Vplus = (1 / k) * (chemo[0] / (chemo[0] + chemo[10] + chemo[11] + chemo[12]))
-    trans_Vminus = (1 / k) * (chemo[0] / (chemo[0] + chemo[6] + chemo[7] + chemo[8]))
-    trans_Hplus_mn = (1 / k) * (chemo[1] / (chemo[1] + chemo[3] + chemo[5] + chemo[9]))
-    trans_Hminus_mn = (1 / k) * (chemo[3] / (chemo[1] + chemo[3] + chemo[5] + chemo[9]))
-    trans_Vplus_mn = (1 / k) * (chemo[5] / (chemo[1] + chemo[3] + chemo[5] + chemo[9]))
-    trans_Vminus_mn = (1 / k) * (chemo[9] / (chemo[1] + chemo[3] + chemo[5] + chemo[9]))
-
-    ntrans_Hplus = trans_Hplus / (trans_Hplus + trans_Hminus + trans_Vplus + trans_Vminus)
-    ntrans_Hminus = trans_Hminus / (trans_Hplus + trans_Hminus + trans_Vplus + trans_Vminus)
-    ntrans_Vplus = trans_Vplus / (trans_Hplus + trans_Hminus + trans_Vplus + trans_Vminus)
-    ntrans_Vminus = trans_Vminus / (trans_Hplus + trans_Hminus + trans_Vplus + trans_Vminus)
-
-    ntrans_Hplus_mn = trans_Hplus_mn / (trans_Hplus_mn + trans_Hminus_mn + trans_Vplus_mn + trans_Vminus_mn)
-    ntrans_Hminus_mn = trans_Hminus_mn / (trans_Hplus_mn + trans_Hminus_mn + trans_Vplus_mn + trans_Vminus_mn)
-    ntrans_Vplus_mn = trans_Vplus_mn / (trans_Hplus_mn + trans_Hminus_mn + trans_Vplus_mn + trans_Vminus_mn)
-    ntrans_Vminus_mn = trans_Vminus_mn / (trans_Hplus_mn + trans_Hminus_mn + trans_Vplus_mn + trans_Vminus_mn)
-
-    delta_pmn = ntrans_Hplus * p_r + ntrans_Hminus * p_l + ntrans_Vplus * p_u + ntrans_Vminus * p_d - (
-                ntrans_Hplus_mn + ntrans_Hminus_mn + ntrans_Vplus_mn + ntrans_Vminus_mn) * p_mn
-
-    p_mn = p_mn + delta_pmn
-    p_r = p_r - ntrans_Hplus * delta_pmn
-    p_l = p_l - ntrans_Hminus * delta_pmn
-    p_u = p_u - ntrans_Vplus * delta_pmn
-    p_d = p_d - ntrans_Vminus * delta_pmn
-
-    rand = random()
-
-    if rand <= p_u:
-        if m != 0:
-            m -= 1
-            workspace[m][n] = 100000
-            memory.add((m, n))
-    elif rand <= p_u + p_d:
-        if m != size-1:
-            m += 1
-            workspace[m][n] = 100000
-            memory.add((m, n))
-    elif rand <= p_u + p_d + p_l:
-        if n != 0:
-            n -= 1
-            workspace[m][n] = 100000
-            memory.add((m, n))
-    elif rand <= p_u + p_d + p_l + p_r:
-        if n != size -1:
-            n += 1
-            workspace[m][n] = 100000
-    else:
-        workspace[m][n] = 100000
+    transitions = transition(chemo, k)
+    normal_transitions = normal_transition(transitions)
+    probabilities = probability(normal_transitions, probabilities)
+    m, n = movement(probabilities, memory, workspace, m, n, size)
 
     iterations += 1
-
 
 plt.imshow(workspace)
 cm.get_cmap("jet")
