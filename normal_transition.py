84e30da293c97acb333dccbcf838439a848e009b
Kelsey <kelsey.mitchell@aggiemail.usu.edu>
2020-01-07 14:49:56 -0700

    this function normalizes the transition probabilities

---
+++ normal_transition.py
@@ -0,0 +1,12 @@
+
+
+def normal_transition(transition):
+    normal_transition = []
+    move_total = transition[0] + transition[1] + transition[2] + transition[3]
+    stay_total = transition[4] + transition[5] + transition[6] + transition[7]
+    for i in range(len(transition)):
+        if i < 4:
+            normal_transition.append(transition[i]/move_total)
+        else:
+            normal_transition.append(transition[i]/stay_total)
+    return normal_transition
