19aa549161c81883894d4ca33cab5fdd9f9a9e34
Kelsey <kelsey.mitchell@aggiemail.usu.edu>
2020-01-08 19:48:05 -0700

    added feature to function if tip cell encounters the capillary it has a 99% chance of survival. Current implementation only works for one tip cell because when a tip cell would die the program just exits. Later will need to just stop tracking one of many tip cells.

--- movement.py
+++ movement.py
@@ -1,8 +1,11 @@
 from random import random
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
 
 def movement(probabilities,memory,workspace,m,n,size):
     rand = random()
+    rand2 = random()
 
     if rand <= probabilities[0] + probabilities[2] + probabilities[3] + probabilities[4]:
         if rand <= probabilities[0]:
@@ -17,6 +20,12 @@ def movement(probabilities,memory,workspace,m,n,size):
         elif rand <= probabilities[0] + probabilities[2] + probabilities[3] + probabilities[4]:
             if m != size - 1:
                 m += 1
+        if (m, n) in memory:
+            if rand2 <= .01:
+                plt.imshow(workspace)
+                cm.get_cmap("jet")
+                plt.show()
+                exit("tip cell died upon encountering capillary")
         memory.add((m, n))
 
     workspace[m][n] = 100000
