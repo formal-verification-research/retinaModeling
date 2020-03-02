529cc4abe2a01f6fc6d0527f3ce461c6f9848100
kelsey <krmmaine@gmail.com>
2020-03-02 14:00:06 -0700

    updated step size to be accurate with the 20micrometer size of a tip cell

--- concentration.py
+++ concentration.py
@@ -5,7 +5,7 @@ import math
 def concentration(m, n, m_source, n_source):
     diffusion_coeff = 0.360                  # mm ^ 2/hr from Fronteirs of oncology paper
     decay = 0.65                             # /hr from Fronteirs of oncology paper
-    step_size = .0032                        # mmfrom Fronteirs of oncology paper
+    step_size = .02                          # mmfrom Fronteirs of oncology paper
     production = 20                          # pg / mm ^ 3 hr Journal of Biological Engineering paper
     radius = spatial.distance.euclidean((m, n), (m_source, n_source))*step_size
     concentration = (production / (2 * (diffusion_coeff * decay) ** 0.5)) * \
