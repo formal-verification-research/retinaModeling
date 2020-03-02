50bdfab51c4268f7fe1dedc7eff9b5bc11d40e5a
kelsey <krmmaine@gmail.com>
2020-03-02 14:58:14 -0700

    I realized that the units weren't working out because I had my production in terms of mass per volume time instead of mass/cross sectional area time. I fixed this by multiplying by the volume and then dividing by the cross sectional area which really came out to multiplying by the stepsize because the height is one mm

--- concentration.py
+++ concentration.py
@@ -5,8 +5,8 @@ import math
 def concentration(m, n, m_source, n_source):
     diffusion_coeff = 0.360                  # mm ^ 2/hr from Fronteirs of oncology paper
     decay = 0.65                             # /hr from Fronteirs of oncology paper
-    step_size = .02                          # mmfrom Fronteirs of oncology paper
-    production = 20                          # pg / mm ^ 3 hr Journal of Biological Engineering paper
+    step_size = .02                          # mm from Fronteirs of oncology paper
+    production = 20 * 0.02                   # pg / mm ^ 2 hr Journal of Biological Engineering paper
     radius = spatial.distance.euclidean((m, n), (m_source, n_source))*step_size
     concentration = (production / (2 * (diffusion_coeff * decay) ** 0.5)) * \
                     (math.exp(-((decay / diffusion_coeff) ** 0.5) * radius))      # concentration in pg / mm3
