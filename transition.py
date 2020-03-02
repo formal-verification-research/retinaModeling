a28afa88cb15dfe24ed955ff2b9581fb11fb4f80
kelsey <krmmaine@gmail.com>
2020-03-02 14:06:01 -0700

    Function now calculates the transition probabilities at the half latice sites provided by the model to be more biologically accurate

--- transition.py
+++ transition.py
@@ -1,13 +1,11 @@
-
-
 def transition(chemo, k):
-    trans_Hplus = (1 / k) * (chemo[0] / (chemo[0] + chemo[4] + chemo[7] + chemo[11]))
-    trans_Hminus = (1 / k) * (chemo[0] / (chemo[0] + chemo[2] + chemo[6] + chemo[10]))
-    trans_Vplus = (1 / k) * (chemo[0] / (chemo[0] + chemo[10] + chemo[11] + chemo[12]))
-    trans_Vminus = (1 / k) * (chemo[0] / (chemo[0] + chemo[6] + chemo[7] + chemo[8]))
-    trans_Hplus_mn = (1 / k) * (chemo[1] / (chemo[1] + chemo[3] + chemo[5] + chemo[9]))
-    trans_Hminus_mn = (1 / k) * (chemo[3] / (chemo[1] + chemo[3] + chemo[5] + chemo[9]))
-    trans_Vplus_mn = (1 / k) * (chemo[5] / (chemo[1] + chemo[3] + chemo[5] + chemo[9]))
-    trans_Vminus_mn = (1 / k) * (chemo[9] / (chemo[1] + chemo[3] + chemo[5] + chemo[9]))
-    return [trans_Hplus, trans_Hminus, trans_Vplus, trans_Vminus, trans_Hplus_mn, trans_Hminus_mn,\
-           trans_Vplus_mn, trans_Vminus_mn]
+    trans_Hplus = (1 / k) * (chemo[7] / (chemo[7] + chemo[10] + chemo[6] + chemo[3]))
+    trans_Hminus = (1 / k) * (chemo[8] / (chemo[8] + chemo[12] + chemo[9] + chemo[5]))
+    trans_Vplus = (1 / k) * (chemo[11] / (chemo[11] + chemo[14] + chemo[15] + chemo[13]))
+    trans_Vminus = (1 / k) * (chemo[4] / (chemo[4] + chemo[1] + chemo[0] + chemo[2]))
+    trans_Hplus_mn = (1 / k) * (chemo[8] / (chemo[8] + chemo[7] + chemo[4] + chemo[11]))
+    trans_Hminus_mn = (1 / k) * (chemo[7] / (chemo[8] + chemo[7] + chemo[4] + chemo[11]))
+    trans_Vplus_mn = (1 / k) * (chemo[4] / (chemo[8] + chemo[7] + chemo[4] + chemo[11]))
+    trans_Vminus_mn = (1 / k) * (chemo[11] / (chemo[8] + chemo[7] + chemo[4] + chemo[11]))
+    return [trans_Hplus, trans_Hminus, trans_Vplus, trans_Vminus, trans_Hplus_mn, trans_Hminus_mn,
+            trans_Vplus_mn, trans_Vminus_mn]
