2fbd0739ebeda8482fc514beac021265d19d70d0
kelsey <krmmaine@gmail.com>
2020-03-18 12:17:32 -0600

    formated concentration function better for more readability and clarity

--- concentration.py
+++ concentration.py
@@ -8,7 +8,7 @@ def concentration(m, n, m_source, n_source):
     step_size = .02                          # mm from Fronteirs of oncology paper
     production = 20 * 0.02                   # pg / mm ^ 2 hr Journal of Biological Engineering paper
     radius = spatial.distance.euclidean((m, n), (m_source, n_source))*step_size
-    concentration = (production / (2 * (diffusion_coeff * decay) ** 0.5)) * \
+    concentration = (production / (2 * ((diffusion_coeff * decay) ** 0.5))) * \
                     (math.exp(-((decay / diffusion_coeff) ** 0.5) * radius))      # concentration in pg / mm3
     return concentration
 
